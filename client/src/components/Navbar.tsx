3884c0a6c9f53da23a256af6fe3647a285c72b3a
Gautam <gautamkumarofficial2309@gmail.com>
2026-01-17 00:21:24 +0530

    update Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -42,7 +42,9 @@ export default function Navbar() {
                                     {user?.name.charAt(0).toUpperCase()}
                                 </button>
                                 <div className="absolute hidden group-hover:block top-6 right-0 pt-4">
-
+                                    <button onClick={()=>logout()} className=" bg-white/20 border white/10 px-5 py-1.5 rounded">
+                                        Logout
+                                    </button>
                                 </div>
                             </div>
                         ): (
@@ -61,8 +63,12 @@ export default function Navbar() {
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <Link onClick={() => setIsOpen(false)} to='/'>Home</Link>
                 <Link onClick={() => setIsOpen(false)} to='/generate' >Generate </Link>
+                {isLoggedIn ? 
                 <Link onClick={() => setIsOpen(false)} to='/my-generation' >My Generation </Link>
-                <Link onClick={() => setIsOpen(false)} to='#' >My Contact </Link>
+                :<Link onClick={() => setIsOpen(false)} to='#' >About</Link>
+                }
+
+                <Link onClick={() => setIsOpen(false)} to='#' >Contact Us </Link>
                 <Link onClick={() => setIsOpen(false)} to='/login' >Login </Link>
 
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
